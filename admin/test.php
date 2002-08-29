4ca88d48240a2ad0aad369714e4da948aeac8231
ipso <ipso@a8c0b1f7-931c-0410-8f5e-c430fc0ac150>
2002-08-29 02:46:04 +0000

    no message

--- admin/test.php
+++ admin/test.php
@@ -2,45 +2,18 @@
 $debug=1;
 require_once("gacl_admin.inc.php");
 
-$group_id = $gacl_api->get_group_id("Browsers");
-$test = $gacl_api->get_group_aro($group_id);
 
-showarray($test);
-
-
-$test = $gacl_api->get_aro_data(10);
-
-showarray($test);
-
-$test = $gacl_api->get_aco_data(10);
-
-showarray($test);
-
-
-$test = $gacl_api->get_aco_data(9999);
-
-showarray($test);
-
-$test = $gacl_api->get_aco_section_id(10);
-
-showarray($test);
-
-$test = $gacl_api->get_aco_section_id(9999);
-
-showarray($test);
-
-
-/*
 //Populate the ARO's
 $max_aros = 100;
 for ($i=0; $i < $max_aros; $i++) {
-	$insert_id = $db->GenID('aro_seq',10);
-	$query = "insert into aro (id,section_id, value,order_value,name) VALUES($insert_id, 41, '$i', '$i', '$i First $i Last')";
-	debug("Query: $query");
 
-	$rs = $db->Execute($query);                   
-	
+	$aro_id = $gacl_api->add_aro(41,"$i First $i Last", $i, $i);
 
+	if ($aro_id) {
+		debug("ARO ID: $aro_id");
+	} else {
+		debug("Insert ARO ID FAILED!");
+	}
 }
-*/
+
 ?>
