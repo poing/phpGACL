9032864df873349b0ade12e1c074cc11df28bc95
ipso <ipso@a8c0b1f7-931c-0410-8f5e-c430fc0ac150>
2003-06-11 23:40:01 +0000

    *** empty log message ***

--- admin/object_search.php
+++ admin/object_search.php
@@ -5,27 +5,55 @@ switch ($_GET['action']) {
     case 'Search':
         $gacl_api->debug_text("Submit!!");
 		
+		//Function to pass array_walk to trim all entries in an array.
+		function array_walk_trim(&$array_field) {
+			$array_field = strtolower(trim($array_field));
+		}
+		
 		$value_search_str = addslashes(trim($_GET['value_search_str']));
 		$name_search_str = addslashes(trim($_GET['name_search_str']));
 	
-		if ($value_search_str != '') {
-			$value_search_str .= '%';
+		$exploded_value_search_str = explode("\n",$value_search_str);
+		$exploded_name_search_str = explode("\n",$name_search_str);
+		
+		if (count($exploded_value_search_str) > 1 OR count($exploded_name_search_str) > 1) {
+			//Given a list, lets try to match all lines in it.
+			array_walk($exploded_value_search_str,"array_walk_trim");
+			array_walk($exploded_name_search_str,"array_walk_trim");
+		} else {
+			if ($value_search_str != '') {
+				$value_search_str .= '%';
+			}
+
+			if ($name_search_str != '') {
+				$name_search_str .= '%';
+			}
 		}
 		
-		if ($name_search_str != '') {
-			$name_search_str .= '%';
-		}
-
 		//Search
         $query = "select 	section_value,
 									value,
 									name
 							from ".$_GET['object_type']."
 							where section_value = '".$_GET['section_value']."'
-								AND 	(
-											lower(value) LIKE '". strtolower($value_search_str) ."'
-												OR lower(name) LIKE '". strtolower($name_search_str) ."'
-											)
+								AND 	";
+		
+		if (count($exploded_value_search_str) > 1 OR count($exploded_name_search_str) > 1) {
+			$query .= "	
+										(
+											lower(value) in ( lower('". implode("'),lower('", $exploded_value_search_str) ."') )
+												OR lower(name) in ( lower('". implode("'),lower('", $exploded_name_search_str) ."') )
+										)
+					";
+		} else {
+			$query .= "
+										(
+											lower(value) LIKE lower('".$value_search_str."')
+												OR lower(name) LIKE lower('".$name_search_str."')
+										)";
+		}
+		
+		$query .= "			
 							order by section_value, order_value
 							limit $gacl_api->_max_search_return_items";
         $rs = $db->Execute($query);
