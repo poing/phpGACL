319ee5b5bed267f43cce9cce2228897821bcc69b
ipso <ipso@a8c0b1f7-931c-0410-8f5e-c430fc0ac150>
2003-07-08 23:26:27 +0000

    *** empty log message ***

--- trunk/adodb/adodb-xmlschema.inc.php
+++ trunk/adodb/adodb-xmlschema.inc.php
@@ -39,22 +39,22 @@ class dbTable {
 	* @var string	Table name
 	*/
 	var $tableName;
-	
+
 	/**
 	* @var array	Field specifier: Meta-information about each field
 	*/
 	var $fieldSpec;
-	
+
 	/**
 	* @var array	Table options: Table-level options
 	*/
 	var $tableOpts;
-	
+
 	/**
 	* @var string	Field index: Keeps track of which field is currently being processed
 	*/
 	var $currentField;
-	
+
 	/**
 	* Constructor. Iniitializes a new table object.
 	*
@@ -94,29 +94,29 @@ class dbTable {
 	* @return array	Field specifier array
 	*/
 	function addField( $name, $type, $size = NULL, $opts = NULL ) {
-	
+
 		// Set the field index so we know where we are
 		$this->currentField = $name;
-		
+
 		// Set the field type (required)
 		$this->fieldSpec[$name]['TYPE'] = $type;
-		
+
 		// Set the field size (optional)
 		if( isset( $size ) ) {
 			$this->fieldSpec[$name]['SIZE'] = $size;
 		}
-		
+
 		// Set the field options
 		if( isset( $opts ) ) $this->fieldSpec[$name]['OPTS'] = $opts;
-		
+
 		// Return array containing field specifier
 		return $this->fieldSpec;
 	}
-	
+
 	/**
 	* Adds a field option to the current field specifier
 	*
-	* This method adds a field option allowed by the ADOdb datadict 
+	* This method adds a field option allowed by the ADOdb datadict
 	* and appends it to the given field.
 	*
 	* @param string $field		Field name
@@ -125,18 +125,18 @@ class dbTable {
 	* @return array	Field specifier array
 	*/
 	function addFieldOpt( $field, $opt, $value = NULL ) {
-		
+
 		// Add the option to the field specifier
 		if(  $value === NULL ) { // No value, so add only the option
 			$this->fieldSpec[$field]['OPTS'][] = $opt;
 		} else { // Add the option and value
 			$this->fieldSpec[$field]['OPTS'][] = array( "$opt" => "$value" );
 		}
-	
+
 		// Return array containing field specifier
 		return $this->fieldSpec;
 	}
-	
+
 	/**
 	* Adds an option to the table
 	*
@@ -147,14 +147,14 @@ class dbTable {
 	* @return string	Option list
 	*/
 	function addTableOpt( $opt ) {
-	
+
 		$optlist = &$this->tableOpts;
 		$optlist ? $optlist .= ", $opt" : $optlist = $opt;
-		
+
 		// Return the options list
 		return $optlist;
 	}
-	
+
 	/**
 	* Generates the SQL that will create the table in the database
 	*
@@ -164,25 +164,25 @@ class dbTable {
 	* @return array	Array containing table creation SQL
 	*/
 	function create( $dict ) {
-	
+
 		// Loop through the field specifier array, building the associative array for the field options
 		$fldarray = array();
 		$i = 0;
-		
+
 		foreach( $this->fieldSpec as $field => $finfo ) {
 			$i++;
-			
+
 			// Set an empty size if it isn't supplied
 			if( !isset( $finfo['SIZE'] ) ) $finfo['SIZE'] = '';
-			
+
 			// Initialize the field array with the type and size
 			$fldarray[$i] = array( $field, $finfo['TYPE'], $finfo['SIZE'] );
-			
-			// Loop through the options array and add the field options. 
+
+			// Loop through the options array and add the field options.
 			$opts = $finfo['OPTS'];
 			if( $opts ) {
 				foreach( $finfo['OPTS'] as $opt ) {
-					
+
 					if( is_array( $opt ) ) { // Option has an argument.
 						$key = key( $opt );
 						$value = $opt[key( $opt ) ];
@@ -193,14 +193,14 @@ class dbTable {
 				}
 			}
 		}
-		
+
 		// Build table array
 		$sqlArray = $dict->CreateTableSQL( $this->tableName, $fldarray, $this->tableOpts );
-		
+
 		// Return the array containing the SQL to create the table
 		return $sqlArray;
 	}
-	
+
 	/**
 	* Destructor
 	*/
@@ -221,22 +221,27 @@ class dbTable {
 * @access private
 */
 class dbIndex {
-	
+
 	/**
 	* @var string	Index name
 	*/
 	var $indexName;
-	
+
+	/**
+	* @var array	Index options: Index-level options
+	*/
+	var $indexOpts;
+
 	/**
 	* @var string	Name of the table this index is attached to
 	*/
 	var $tableName;
-	
+
 	/**
 	* @var array	Indexed fields: Table columns included in this index
 	*/
 	var $fields;
-	
+
 	/**
 	* Constructor. Initialize the index and table names.
 	*
@@ -247,10 +252,10 @@ class dbIndex {
 		$this->indexName = $name;
 		$this->tableName = $table;
 	}
-	
+
 	/**
 	* Adds a field to the index
-	* 
+	*
 	* This method adds the specified column to an index.
 	*
 	* @param string $name		Field name
@@ -259,11 +264,29 @@ class dbIndex {
 	function addField( $name ) {
 		$fieldlist = &$this->fields;
 		$fieldlist ? $fieldlist .=" , $name" : $fieldlist = $name;
-		
+
 		// Return the field list
 		return $fieldlist;
 	}
-	
+
+	/**
+	* Adds an option to the index
+	*
+	*This method takes a comma-separated list of index-level options
+	* and appends them to the index object.
+	*
+	* @param string $opt		Index option
+	* @return string	Option list
+	*/
+	function addIndexOpt( $opt ) {
+
+		$optlist = &$this->indexOpts;
+		$optlist ? $optlist .= ", $opt" : $optlist = $opt;
+
+		// Return the options list
+		return $optlist;
+	}
+
 	/**
 	* Generates the SQL that will create the index in the database
 	*
@@ -273,14 +296,19 @@ class dbIndex {
 	* @return array	Array containing index creation SQL
 	*/
 	function create( $dict ) {
-	
+
+		if (isset($this->indexOpts) ) {
+			// CreateIndexSQL requires an array of options.
+			$indexOpts_arr = explode(",",$this->indexOpts);
+		}
+
 		// Build table array
-		$sqlArray = $dict->CreateIndexSQL( $this->indexName, $this->tableName, $this->fields );
-		
+		$sqlArray = $dict->CreateIndexSQL( $this->indexName, $this->tableName, $this->fields, $indexOpts_arr );
+
 		// Return the array containing the SQL to create the table
 		return $sqlArray;
 	}
-	
+
 	/**
 	* Destructor
 	*/
@@ -298,17 +326,17 @@ class dbIndex {
 * @access private
 */
 class dbQuerySet {
-	
+
 	/**
 	* @var array	List of SQL queries
 	*/
 	var $querySet;
-	
+
 	/**
 	* @var string	String used to build of a query line by line
 	*/
 	var $query;
-	
+
 	/**
 	* Constructor. Initializes the queries array
 	*/
@@ -316,8 +344,8 @@ class dbQuerySet {
 		$this->querySet = array();
 		$this->query = '';
 	}
-	
-	/** 
+
+	/**
 	* Appends a line to a query that is being built line by line
 	*
 	* $param string $data	Line of SQL data or NULL to initialize a new query
@@ -325,14 +353,14 @@ class dbQuerySet {
 	function buildQuery( $data = NULL ) {
 		isset( $data ) ? $this->query .= " " . trim( $data ) : $this->query = '';
 	}
-	
+
 	/**
 	* Adds a completed query to the query list
 	*
 	* @return string	SQL of added query
 	*/
 	function addQuery() {
-		
+
 		// Push the query onto the query set array
 		$finishedQuery = $this->query;
 		array_push( $this->querySet, $finishedQuery );
@@ -361,88 +389,88 @@ class dbQuerySet {
 
 /**
 * Loads and parses an XML file, creating an array of "ready-to-run" SQL statements
-* 
+*
 * This class is used to load and parse the XML file, to create an array of SQL statements
 * that can be used to build a database, and to build the database using the SQL array.
 *
 * @package xmlschema
 */
 class adoSchema {
-	
+
 	/**
 	* @var array	Array containing SQL queries to generate all objects
 	*/
 	var $sqlArray;
-	
+
 	/**
 	* @var object	XML Parser object
 	*/
 	var $xmlParser;
-	
+
 	/**
 	* @var object	ADOdb connection object
 	*/
 	var $dbconn;
-	
+
 	/**
 	* @var string	Database type (platform)
 	*/
 	var $dbType;
-	
+
 	/**
 	* @var object	ADOdb Data Dictionary
 	*/
 	var $dict;
-	
+
 	/**
 	* @var object	Temporary dbTable object
 	* @access private
 	*/
 	var $table;
-	
+
 	/**
 	* @var object	Temporary dbIndex object
 	* @access private
 	*/
 	var $index;
-	
+
 	/**
 	* @var object	Temporary dbQuerySet object
 	* @access private
 	*/
 	var $querySet;
-	
+
 	/**
 	* @var string Current XML element
 	* @access private
 	*/
 	var $currentElement;
-	
+
 	/**
 	* @var long	Original Magic Quotes Runtime value
 	* @access private
 	*/
 	var $mgq;
-	
+
 	/**
 	* Constructor. Initializes the xmlschema object
 	*
 	* @param object $dbconn		ADOdb connection object
 	*/
 	function adoSchema( $dbconn ) {
-		
+
 		// Initialize the environment
 		$this->mgq = get_magic_quotes_runtime();
-		set_magic_quotes_runtime(0);	
-		
+		set_magic_quotes_runtime(0);
+
 		$this->dbconn	=  &$dbconn;
 		$this->dbType = $dbconn->databaseType;
 		$this->sqlArray = array();
-		
+
 		// Create an ADOdb dictionary object
 		$this->dict = NewDataDictionary( $dbconn );
 	}
-	
+
 	/**
 	* Loads and parses an XML file
 	*
@@ -454,21 +482,21 @@ class adoSchema {
 	* @return array	Array of SQL queries, ready to execute
 	*/
 	function ParseSchema( $file ) {
-	
+
 		// Create the parser
 		$this->xmlParser = &$xmlParser;
 		$xmlParser = xml_parser_create();
 		xml_set_object( $xmlParser, &$this );
-		
+
 		// Initialize the XML callback functions
 		xml_set_element_handler( $xmlParser, "_xmlcb_startElement", "_xmlcb_endElement" );
 		xml_set_character_data_handler( $xmlParser, "_xmlcb_cData" );
-		
+
 		// Open the file
 		if( !( $fp = fopen( $file, "r" ) ) ) {
 			die( "Unable to open file" );
 		}
-		
+
 		// Process the file
 		while( $data = fread( $fp, 4096 ) ) {
 			if( !xml_parse( $xmlParser, $data, feof( $fp ) ) ) {
@@ -477,15 +505,15 @@ class adoSchema {
 					xml_get_current_line_number( $xmlParser ) ) );
 			}
 		}
-		
+
 		// Return the array of queries
 		return $this->sqlArray;
 	}
-	
+
 	/**
 	* Loads a schema into the database
 	*
-	* Accepts an array of SQL queries generated by the parser 
+	* Accepts an array of SQL queries generated by the parser
 	* and executes them.
 	*
 	* @param array $sqlArray	Array of SQL statements
@@ -494,31 +522,39 @@ class adoSchema {
 	*/
 	function ExecuteSchema( $sqlArray, $continueOnErr =  TRUE ) {
 		$err = $this->dict->ExecuteSQLArray( $sqlArray, $continueOnErr );
-		
+
 		// Return the success code
 		return $err;
 	}
-	
+
 	/**
 	* XML Callback to process start elements
 	*
 	* @access private
 	*/
 	function _xmlcb_startElement( $parser, $name, $attrs ) {
-		
+
 		$dbType = $this->dbType;
 		if( isset( $this->table ) ) $table = &$this->table;
 		if( isset( $this->index ) ) $index = &$this->index;
 		if( isset( $this->querySet ) ) $querySet = &$this->querySet;
 		$this->currentElement = $name;
-		
+
 		// Process the element. Ignore unimportant elements.
 		if( in_array( trim( $name ),  array( "SCHEMA", "DESCR", "COL", "CONSTRAINT" ) ) ) {
 			return FALSE;
 		}
-		
+
 		switch( $name ) {
-				
+
+			case "CLUSTERED":	// IndexOpt
+			case "BITMAP":		// IndexOpt
+			case "UNIQUE":		// IndexOpt
+			case "FULLTEXT":	// IndexOpt
+			case "HASH":		// IndexOpt
+				if( isset( $this->index ) ) $this->index->addIndexOpt( $name );
+				break;
+
 			case "TABLE":	// Table element
 				if( $this->supportedPlatform( $attrs['PLATFORM'] ) ) {
 					$this->table = new dbTable( $attrs['NAME'] );
@@ -526,7 +562,7 @@ class adoSchema {
 					unset( $this->table );
 				}
 				break;
-				
+
 			case "FIELD":	// Table field
 				if( isset( $this->table ) ) {
 					$fieldName = $attrs['NAME'];
@@ -536,31 +572,31 @@ class adoSchema {
 					$this->table->addField( $fieldName, $fieldType, $fieldSize, $fieldOpts );
 				}
 				break;
-				
+
 			case "KEY":	// Table field option
 				if( isset( $this->table ) ) {
 					$this->table->addFieldOpt( $this->table->currentField, 'KEY' );
 				}
 				break;
-				
+
 			case "NOTNULL":	// Table field option
 				if( isset( $this->table ) ) {
 					$this->table->addFieldOpt( $this->table->currentField, 'NOTNULL' );
 				}
 				break;
-				
+
 			case "AUTOINCREMENT":	// Table field option
 				if( isset( $this->table ) ) {
 					$this->table->addFieldOpt( $this->table->currentField, 'AUTOINCREMENT' );
 				}
 				break;
-				
+
 			case "DEFAULT":	// Table field option
 				if( isset( $this->table ) ) {
 					$this->table->addFieldOpt( $this->table->currentField, 'DEFAULT', $attrs['VALUE'] );
 				}
 				break;
-				
+
 			case "INDEX":	// Table index
 				if( $this->supportedPlatform( $attrs['PLATFORM'] ) ) {
 					$this->index = new dbIndex( $attrs['NAME'], $attrs['TABLE'] );
@@ -568,7 +604,7 @@ class adoSchema {
 					if( isset( $this->index ) ) unset( $this->index );
 				}
 				break;
-				
+
 			case "SQL":	// Freeform SQL queryset
 				if( $this->supportedPlatform( $attrs['PLATFORM'] ) ) {
 					$this->querySet = new dbQuerySet( $attrs );
@@ -576,10 +612,10 @@ class adoSchema {
 					if( isset( $this->querySet ) ) unset( $this->querySet );
 				}
 				break;
-				
+
 			case "QUERY":	// Queryset SQL query
 				if( isset( $this->querySet ) ) {
-					// Ignore this query set if a platform is specified and it's different than the 
+					// Ignore this query set if a platform is specified and it's different than the
 					// current connection platform.
 					if( $this->supportedPlatform( $attrs['PLATFORM'] ) ) {
 						$this->querySet->buildQuery();
@@ -588,10 +624,10 @@ class adoSchema {
 					}
 				}
 				break;
-				
+
 			default:
 				print "OPENING ELEMENT '$name'<BR/>\n";
-		}	
+		}
 	}
 
 	/**
@@ -600,18 +636,18 @@ class adoSchema {
 	* @access private
 	*/
 	function _xmlcb_cData( $parser, $data ) {
-		
+
 		$element = &$this->currentElement;
-		
+
 		if( trim( $data ) == "" ) return;
-		
+
 		// Process the data depending on the element
 		switch( $element ) {
-		
+
 			case "COL":	// Index column
 				if( isset( $this->index ) ) $this->index->addField( $data );
 				break;
-				
+
 			case "DESCR":	// Description element
 				// Display the description information
 				if( isset( $this->table ) ) {
@@ -623,15 +659,15 @@ class adoSchema {
 				}
 				print "<LI> $name $data\n";
 				break;
-			
+
 			case "QUERY":	// Query SQL data
 				if( isset( $this->querySet ) and isset( $this->querySet->query ) ) $this->querySet->buildQuery( $data );
 				break;
-			
+
 			case "CONSTRAINT":	// Table constraint
 				if( isset( $this->table ) ) $this->table->addTableOpt( $data );
 				break;
-				
+
 			default:
 				print "<UL><LI>CDATA ($element) $data</UL>\n";
 		}
@@ -643,16 +679,16 @@ class adoSchema {
 	* @access private
 	*/
 	function _xmlcb_endElement( $parser, $name ) {
-		
+
 		// Process the element. Ignore unimportant elements.
-		if( in_array( trim( $name ), 
+		if( in_array( trim( $name ),
 			array( 	"SCHEMA", "DESCR", "KEY", "AUTOINCREMENT", "FIELD",
 						"DEFAULT", "NOTNULL", "CONSTRAINT", "COL" ) ) ) {
 			return FALSE;
 		}
-		
+
 		switch( trim( $name ) ) {
-			
+
 			case "TABLE":	// Table element
 				if( isset( $this->table ) ) {
 					$tableSQL = $this->table->create( $this->dict );
@@ -660,7 +696,7 @@ class adoSchema {
 					$this->table->destroy();
 				}
 				break;
-				
+
 			case "INDEX":	// Index element
 				if( isset( $this->index ) ) {
 					$indexSQL = $this->index->create( $this->dict );
@@ -668,11 +704,11 @@ class adoSchema {
 					$this->index->destroy();
 				}
 				break;
-			
+
 			case "QUERY":	// Queryset element
 				if( isset( $this->querySet ) and isset( $this->querySet->query ) ) $this->querySet->addQuery();
 				break;
-			
+
 			case "SQL":	// Query SQL element
 				if( isset( $this->querySet ) ) {
 					$querySQL = $this->querySet->create();
