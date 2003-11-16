0301bf0f919bfe373cee58ae500b33ceb515ea00
dcech <dcech@a8c0b1f7-931c-0410-8f5e-c430fc0ac150>
2003-11-16 18:30:18 +0000

    merge from HEAD

--- branches/dcech/test_suite/run.php
+++ branches/dcech/test_suite/run.php
@@ -1,30 +1,36 @@
 <?php
-
-require_once("unit_tests.php");
-
-$title = 'phpGACL Test Suite';
+	// require gacl & phpunit
+	require_once(dirname(__FILE__).'/../admin/gacl_admin.inc.php');
+	require_once(dirname(__FILE__).'/phpunit/phpunit.php');
+	
+	$title = 'phpGACL Test Suite';
 ?>
   <html>
     <head>
       <title><?php echo $title; ?></title>
       <STYLE TYPE="text/css">
-	<?php
-	include ("phpunit/stylesheet.css");
-	?>
+<?php
+	include ('phpunit/stylesheet.css');
+?>
       </STYLE>
     </head>
     <body>
       <div align="center"><h1><b><?php echo $title; ?></b></h1></div>
       <div align="center"><h3><b>You may have to run these tests several times at first.</b></h3></div>
-      <p>
-	<?php
-	if (isset($only)) {
-	$suite = new TestSuite($only);
-	}
-
+<?php
+	// initialise suite
+	$suite = new TestSuite;
 	$result = new PrettyTestResult;
+	
+	// get api tests
+	include('unit_tests.php');
+	
+	// get acl tests
+	// include('acl_tests.php');
+	
+	// run suite
 	$suite->run($result);
 	$result->report();
-	?>
+?>
     </body>
   </html>
